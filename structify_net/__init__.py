d7a46dbf3ab5451201d5e28864397cf561ac6311
Cazabet Remy <remy.cazabet@gmail.com>
2023-04-18 22:36:47 +0200

    Update __init__.py

--- structify_net/__init__.py
+++ structify_net/__init__.py
@@ -1,5 +1,4 @@
 from structify_net.viz import plot_adjacency_matrix,spider_plot
-from structify_net import test
+from structify_net import scoring
 from structify_net.structureClasses import Rank_model,Graph_generator
 from structify_net import zoo
-from structify_net import scoring
