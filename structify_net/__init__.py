b4cff8492f15270b320867ff59081abe1383318c
Cazabet Remy <remy.cazabet@gmail.com>
2023-04-18 12:49:41 +0200

    clean import

--- structify_net/__init__.py
+++ structify_net/__init__.py
@@ -1,3 +1,2 @@
-from structify_net.viz import *
-from structify_net.transform import *
-from structify_net.structureClasses import *
+from structify_net.viz import plot_adjacency_matrix,spider_plot
+from structify_net.structureClasses import RankModel,Graph_generator
