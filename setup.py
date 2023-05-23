99876dc730738688f49935f37c6ab373adf5a5b4
Cazabet Remy <remy.cazabet@gmail.com>
2023-05-23 18:16:08 +0200

    sfd

--- setup.py
+++ setup.py
@@ -1,43 +1,8 @@
 from setuptools import setup, find_packages
-from codecs import open
-from os import path
 
 __author__ = 'Remy Cazabet'
 __license__ = "BSD-2-Clause"
 __email__ = "remy.cazabet@gmail.com"
-
-
-def get_requirements(remove_links=True):
-    """
-    lists the requirements to install.
-    """
-
-    try:
-        with open('requirements.txt') as f:
-            requirements = f.read().splitlines()
-    except Exception as ex:
-        with open('DecoraterBotUtils.egg-info\requires.txt') as f:
-            requirements = f.read().splitlines()
-    if remove_links:
-        for requirement in requirements:
-            # git repository url.
-            if requirement.startswith("git+"):
-                requirements.remove(requirement)
-            # subversion repository url.
-            if requirement.startswith("svn+"):
-                requirements.remove(requirement)
-            # mercurial repository url.
-            if requirement.startswith("hg+"):
-                requirements.remove(requirement)
-    return requirements
-
-
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
 
 setup(name='structify_net',
       license='BSD-Clause-2',
@@ -64,7 +29,7 @@ setup(name='structify_net',
           'Programming Language :: Python :: 3'
       ],
       keywords='network-science community-detection',
-      install_requires=get_requirements(),
-      packages=find_packages(exclude=["*.test", "*.test.*", "test.*", "test"]),
+      install_requires=[],
+      packages=find_packages(),
       include_package_data=True
       )
